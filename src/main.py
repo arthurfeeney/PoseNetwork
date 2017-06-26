fixed normalizing the quaternion in the loss function.

src/main.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from net import decode_dual_stream
+from net import shared_dual_stream, decode_dual_stream
 from container import Data
 from scenesDownload import *
 from inception_resnet_v2 import *
@@ -42,8 +42,7 @@ def main():
         exclude = ['InceptionResnetV2/Logits', 'InceptionResnetV2/AuxLogits']
         variables_to_restore = slim.get_variables_to_restore(exclude = exclude)
 
-
-        decode_dual_stream(end_points, lr=1e-4)
+        shared_dual_stream(end_points, lr=1e-4)
 
         end_points['input_tensor'] = input_tensor
         end_points['feed_tensor'] = feed_tensor
@@ -57,14 +56,14 @@ def main():
           '/data/zhanglab/afeeney/inception_resnet_v2_2016_08_30.ckpt',
           data,
           batch_size=32,
-          num_epochs=20,
+          num_epochs=80,
           verbose=True)
 
     print('finished training')
 
     error = test(end_points, data)
 
-    print('distance' + str(error[0]) + ' angle ' + str(error[1]))
+    print('distance: ' + str(error[0]) + ' angle: ' + str(error[1]))
 
 def feed_helper(end_points,
                 images,
@@ -95,7 +94,8 @@ def feed_helper(end_points,
                     }
                 )
             }
-        ), end_points['labels']: labels
+        ),
+        end_points['labels']: labels
     }
 
 def train(end_points,
@@ -117,20 +117,21 @@ def train(end_points,
         loader = tf.train.Saver(variables_to_restore)
         loader.restore(sess, checkpoint_file)
 
-        for i in range(num_epochs):
-            for index in range(0, data.train_size()-batch_size-1, batch_size):
+        for epoch in range(num_epochs):
+            for step in range(0, data.train_size()-batch_size-1, batch_size):
                 data.shuffle()
 
                 images, labels = data.get_next_batch(batch_size)
 
-                if verbose and index % (2 * batch_size) == 0:
+                if verbose and step % (2 * batch_size) == 0:
                     batch_acc = end_points['acc'].eval(
                         feed_dict=feed_helper(
                                     end_points,
                                     images,
                                     labels)
                     )
-                    print('step: '+str(index)+' acc: '+str(batch_acc))
+                    print('epoch: ' + str(epoch) + ' step: ' + \
+                          str(step) + ' acc: ' + str(batch_acc))
 
                 end_points['step'].run(
                     feed_dict=feed_helper(end_points, images, labels)

src/net.py
@@ -14,26 +14,58 @@ def euclidean_distance(predicted, actual, scale = 10):
 
 # from geometric loss functions paper
 def distance_with_learned_scale(predicted, actual):
+    """
+    loss function learns a scale between position and orientation.
+    Use axis one so that the function is applied to each element of the batch
+    and not to the batch itself.
+    """
     position, orientation = tf.split(predicted, [4,5], axis=1)
 
     x, s_x = tf.split(position, [3,1], axis=1)
 
-    q, s_q = tf.split(orientation, [4, 1], axis=1)
+    q, s_q = tf.split(orientation, [4,1], axis=1)
 
     x_, q_ = tf.split(actual, [3,4], axis=1)
 
-    left = tf.add(tf.multiply(tf.norm(x_ - x), tf.exp(-s_x)), s_x)
+    left = tf.add(tf.multiply(tf.norm(tf.subtract(x_, x), axis=1),
+                    tf.exp(-s_x)), s_x)
 
-    unit_q = tf.divide(q, tf.norm(q))
+    norm_q = tf.norm(q, axis=1, keep_dims=True)
 
-    right = tf.add(tf.multiply(tf.norm(q_ - unit_q), tf.exp(-s_q)), s_q)
+    unit_q = tf.divide(q, norm_q)
 
-    return left + right
+    #arr = tf.TensorArray(tf.float32, 1, dynamic_size=True, infer_shape=False)
 
+    #l_q = arr.unstack(q)
+    #q_norms = [tf.norm(qwop) for qwop in l_q]
+    #unit_q = l_q.stack(q / q_norms)
+
+    right = tf.add(
+                tf.multiply(
+                    tf.norm(tf.subtract(q_, unit_q), axis=1),
+                    tf.exp(-s_q)
+                ),
+                s_q
+            )
+
+    return tf.add(left, right)
+
+def _get_angle(q):
+    qr, rest = tf.split(q, [1,3], axis=1)
+
+    left_arg = tf.sqrt(tf.reduce_sum(tf.square(rest)))
+
+    angle = tf.multiply(2.0, tf.atan2(left_arg, qr))
+
+    return angle
 
 def position_and_angle(predicted, actual):
-    position, orientation = \
-        tf.split(predicted, [4,5], axis=1)
+    """
+    computes the position and angle error for printing.
+    it uses axis 1 to apply function to each element of batch.
+    The math functions should all be element wise already.
+    """
+    position, orientation = tf.split(predicted, [4,5], axis=1)
 
     x, _ = tf.split(position, [3,1], axis=1)
 
@@ -41,19 +73,108 @@ def position_and_angle(predicted, actual):
 
     x_, q_ = tf.split(actual, [3,4], axis=1)
 
-    distance = tf.norm(x_ - x)
+    distance = tf.norm(tf.subtract(x_, x), axis=1)
 
-    unit_q = tf.divide(q, tf.norm(q))
+    #arr = tf.TensorArray(tf.float32, 1, dynamic_size=True, infer_shape=False)
+    #l_q = arr.unstack(q)
+    #q_norms = [tf.norm(qwop) for qwop in l_q]
+    #unit_q = l_q.stack(q / q_norms)
 
-    #cos_angle = tf.reduce_sum(tf.multiply(unit_q, q))
-    #angle = tf.acos(tf.clip_by_value(cos_angle, -1.0, 1.0))
-    #angle_degrees = tf.divide(tf.multiply(angle, 180), np.pi)
+    norm_q = tf.norm(q, axis=1, keep_dims=True)
 
-    angle = tf.norm(q_ - unit_q)
+    unit_q = tf.divide(q, norm_q)
+
+    angle = tf.abs(tf.subtract(_get_angle(q_), _get_angle(unit_q)))
+    angle = tf.divide(tf.multiply(angle, 180.0), np.pi) # to degrees
 
     return tf.stack(
-            (tf.reduce_mean(distance), tf.reduce_mean(angle)),
-            axis=0)
+                (tf.reduce_mean(distance), tf.reduce_mean(angle)),
+                axis=0
+           )
+
+
+def shared_dual_stream(model, lr=1e-3):
+
+    net = model['PrePool']
+
+    model['upper_input'] = tf.placeholder(tf.float32, shape=net.get_shape())
+
+    net = slim.conv2d_transpose(model['upper_input'],
+                                tf.Dimension(int(net.get_shape()[3])),
+                                (4,4),
+                                stride=2,
+                                normalizer_fn=slim.batch_norm)
+
+    net = slim.conv2d_transpose(net,
+                                tf.Dimension(int(net.get_shape()[3])/2),
+                                (4,4),
+                                stride=2,
+                                normalizer_fn=slim.batch_norm)
+
+    net = slim.conv2d_transpose(net,
+                                tf.Dimension(int(net.get_shape()[3])/2),
+                                (4,4),
+                                stride=2,
+                                normalizer_fn=slim.batch_norm)
+
+    left_1 = slim.conv2d(net,
+                         32,
+                         (3,3),
+                         stride=1,
+                         normalizer_fn=slim.batch_norm)
+
+    right_1 = slim.conv2d(net,
+                          32,
+                          (3,3),
+                          stride=1,
+                          normalizer_fn=slim.batch_norm)
+
+    left_and_share_right = tf.add(left_1, tf.multiply(right_1, 0.7))
+
+    right_and_share_left = tf.add(right_1, tf.multiply(left_1, 0.7))
+
+    loc_input = slim.flatten(left_and_share_right)
+
+    ori_input = slim.flatten(right_and_share_left)
+
+    loc_1 = slim.fully_connected(loc_input,
+                                 1024,
+                                 normalizer_fn=slim.batch_norm)
+
+    ori_1 = slim.fully_connected(ori_input,
+                                 1024,
+                                 normalizer_fn=slim.batch_norm)
+
+    loc_and_share_ori = tf.add(loc_1, tf.multiply(ori_1, 0.7))
+
+    ori_and_share_loc = tf.add(ori_1, tf.multiply(loc_1, 0.7))
+
+    loc = slim.fully_connected(loc_and_share_ori,
+                               4,
+                               normalizer_fn=slim.batch_norm)
+
+    ori = slim.fully_connected(ori_and_share_loc,
+                               5,
+                               normalizer_fn=slim.batch_norm)
+
+    logits = tf.concat((loc, ori), axis=1)
+
+    loss = distance_with_learned_scale(
+        predicted=logits,
+        actual=model['labels']
+    )
+
+    distance = position_and_angle(
+        predicted=logits,
+        actual=model['labels']
+    )
+
+    model['step'] = tf.train.AdamOptimizer(
+        learning_rate=lr
+    ).minimize(loss)
+
+    model['acc'] = distance
+
 
 def decode_dual_stream(model, lr=1e-3):
 
@@ -68,7 +189,7 @@ def decode_dual_stream(model, lr=1e-3):
                                 normalizer_fn=slim.batch_norm)
 
     net = slim.conv2d_transpose(net,
-                                tf.Dimension(int(net.get_shape()[3])/2),
+                                tf.Dimension(int(net.get_shape()[3])),
                                 (4,4),
                                 stride=2,
                                 normalizer_fn=slim.batch_norm)
@@ -95,6 +216,10 @@ def decode_dual_stream(model, lr=1e-3):
 
     ori = slim.flatten(ori)
 
+    loc = slim.fully_connected(loc, 1024, normalizer_fn=slim.batch_norm)
+
+    ori = slim.fully_connected(ori, 1024, normalizer_fn=slim.batch_norm)
+
     loc = slim.fully_connected(loc, 4, normalizer_fn=slim.batch_norm)
 
     ori = slim.fully_connected(ori, 5, normalizer_fn=slim.batch_norm)
@@ -106,7 +231,7 @@ def decode_dual_stream(model, lr=1e-3):
         actual=model['labels']
     )
 
-    distance = position_and_angle(
+    model['acc'] = position_and_angle(
         predicted=logits,
         actual=model['labels']
     )
@@ -114,6 +239,4 @@ def decode_dual_stream(model, lr=1e-3):
     model['step'] = tf.train.AdamOptimizer(
         learning_rate=lr
     ).minimize(loss)
-
-    model['acc'] = distance
 