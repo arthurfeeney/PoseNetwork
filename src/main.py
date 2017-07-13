made fix for matrix to quaternion, doesnt use slim anymore.

src/main.py
@@ -42,8 +42,6 @@ def main():
         exclude = ['InceptionResnetV2/Logits', 'InceptionResnetV2/AuxLogits']
         variables_to_restore = slim.get_variables_to_restore(exclude=exclude)
 
-        decode_dual_stream(end_points, lr=1e-4)
-
         end_points['input_tensor'] = input_tensor
         end_points['feed_tensor'] = feed_tensor
 
@@ -54,7 +52,7 @@ def main():
           checkpoint_file,
           data,
           batch_size=40,
-          num_epochs=20,
+          num_epochs=120,
           verbose=True)
 
     print('finished training')
@@ -70,6 +68,7 @@ def main():
 def feed_helper(end_points,
                 images,
                 labels,
+                is_training=True,
                 keep_prob=0.5,
                 random_crop=True,
                 image_size=299):
@@ -100,7 +99,8 @@ def feed_helper(end_points,
             }
         ),
         end_points['labels']: labels,
-        end_points['keep_prob']: keep_prob
+        end_points['keep_prob']: keep_prob,
+        end_points['is_training']: is_training
     }
 
 def train(end_points,
@@ -111,6 +111,8 @@ def train(end_points,
           num_epochs=1,
           batch_size=32,
           verbose=False):
+    with tf.device('/gpu:0'):
+        update = decode_dual_stream(end_points)
     with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
 
         sess.run(tf.global_variables_initializer())
@@ -136,7 +138,7 @@ def train(end_points,
                     print('epoch: ' + str(epoch) + ' step: ' + \
                           str(step) + ' acc: ' + str(batch_acc))
 
-                end_points['step'].run(
+                update.run(
                     feed_dict=feed_helper(end_points, images, labels)
                 )
 
@@ -149,10 +151,10 @@ def test(end_points,
          image_size=299,
          batch_size=1,
          verbose=False):
+    loader = tf.train.Saver()
+    with tf.device('/gpu:0'):
+        _ = decode_dual_stream(end_points, reuse=True)
     with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
-        loader = tf.train.import_meta_graph(
-            '/data/zhanglab/afeeney/chess_test-0.meta'
-        )
         loader.restore(sess, tf.train.latest_checkpoint(
             '/data/zhanglab/afeeney/'
         ))
@@ -171,7 +173,7 @@ def test(end_points,
             acc += end_points['acc'].eval(
                 feed_dict=feed_helper(end_points, images, labels,
                                       random_crop=False, image_size=image_size,
-                                      keep_prob=1.0)
+                                      keep_prob=1.0, is_training=False)
             )
 
             if verbose:

src/net.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
-slim = tf.contrib.slim
+layers = tf.contrib.layers
 
 def euclidean_distance(predicted, actual, scale = 6):
     """
@@ -140,7 +140,7 @@ def shared_dual_stream(model, lr=1e-4):
 
     model['acc'] = distance
 
-def decode_dual_stream(model, lr=1e-4):
+def decode_dual_stream(model, lr=1e-4, reuse=None):
     """
     Implementation of network from the hourglass networks paper.
     """
@@ -150,45 +150,68 @@ def decode_dual_stream(model, lr=1e-4):
 
     model['keep_prob'] = tf.placeholder(tf.float32)
 
-    with slim.arg_scope([slim.conv2d_transpose, slim.conv2d],
-                        normalizer_fn=slim.batch_norm):
+    model['is_training'] = tf.placeholder(tf.bool)
 
-        net = slim.conv2d_transpose(model['upper_input'], net.get_shape()[3],
+    with tf.variable_scope('model', reuse=reuse):
+
+        net = layers.conv2d_transpose(model['upper_input'], net.get_shape()[3],
                                     (4,4), stride=2)
+        net = layers.batch_norm(net, decay=0.99, updates_collections=None,
+                                reuse=reuse, scope='b1',
+                                is_training=model['is_training'])
+        net = tf.nn.relu(net)
 
-        net = slim.conv2d_transpose(net, 768, (4,4), stride=2)
+        net = layers.conv2d_transpose(net, 768, (4,4), stride=2)
 
-        net = slim.conv2d_transpose(net, 384, (4,4), stride=2)
+        net = layers.batch_norm(net, decay=0.99, updates_collections=None,
+                                reuse=reuse, scope='b2',
+                                is_training=model['is_training'])
+        net = tf.nn.relu(net)
 
-        net = slim.conv2d(net, 32, (3,3), stride=1, padding='VALID')
+        net = layers.conv2d_transpose(net, 384, (4,4), stride=2)
+        net = layers.batch_norm(net, decay=0.99, updates_collections=None,
+                                reuse=reuse, scope='b3',
+                                is_training=model['is_training'])
+        net = tf.nn.relu(net)
 
-        net = slim.flatten(net)
+        net = layers.conv2d(net, 32, (3,3), stride=1, padding='VALID')
+        net = layers.batch_norm(net, decay=0.99, updates_collections=None,
+                                reuse=reuse, scope='b4',
+                                is_training=model['is_training'])
+        net = tf.nn.relu(net)
 
-    net = slim.fully_connected(net, 1024)
+        net = layers.flatten(net)
 
-    net = slim.dropout(net, keep_prob=model['keep_prob'])
+        net = tf.layers.dense(net, 1024)
+        net = layers.batch_norm(net, decay=0.99, updates_collections=None,
+                                reuse=reuse, scope='b5',
+                                is_training=model['is_training'])
+        net = tf.nn.relu(net)
 
-    with slim.arg_scope([slim.fully_connected], activation_fn=None):
+        net = layers.dropout(net, keep_prob=model['keep_prob'])
 
-        loc = slim.fully_connected(net, 3)
+        loc = tf.layers.dense(net, 3)
 
-        ori = slim.fully_connected(net, 4)
+        ori = tf.layers.dense(net, 4)
 
-    logits = tf.concat((loc, ori), axis=1)
+        logits = tf.concat((loc, ori), axis=1)
 
-    s_x = tf.Variable(0.0, dtype=tf.float32, trainable=True)
+        s_x = tf.Variable(0.0, dtype=tf.float32, trainable=True)
 
-    s_q = tf.Variable(-3.0, dtype=tf.float32, trainable=True)
+        s_q = tf.Variable(-3.0, dtype=tf.float32, trainable=True)
 
-    loss = _distance_with_learned_scale(predicted=logits,
+        loss = _distance_with_learned_scale(predicted=logits,
                                         actual=model['labels'],
                                         s_x=s_x, s_q=s_q)
 
-    model['acc'] = _position_and_angle(predicted=logits,
+        model['acc'] = _position_and_angle(predicted=logits,
                                        actual=model['labels'])
 
-    model['step'] = tf.train.AdamOptimizer(
-        learning_rate=lr,
-        epsilon=1e-6
-    ).minimize(loss)
+    update = 'qwop'
+    if reuse is None:
+        update = tf.train.AdamOptimizer(
+            learning_rate=lr,
+            epsilon=1e-6
+        ).minimize(loss)
+    return update
 

src/scenes_download.py
@@ -34,8 +34,7 @@ def _load_data(filename):
     # Load an image
     load_image = Image.open(data_path + filename)
 
-    #rescale the image.
-
+    #rescale the image so the smallest side is 299
     re_image = load_image.resize((399, 299))
 
     # convert image to a numpy array
@@ -68,7 +67,7 @@ def _load_pls(filename):
     trace = m[0][0] + m[1][1] + m[2][2]
 
     if trace > 0:
-        s = .5
+        s = .5 / math.sqrt(trace + 1.0)
         q[0] = .25 / s
         q[1] = (m[2][1] - m[1][2]) * s
         q[2] = (m[0][2] - m[2][0]) * s
@@ -92,8 +91,6 @@ def _load_pls(filename):
             q[1] = (m[0][2] + m[2][0]) / s
             q[2] = (m[1][2] + m[2][1]) / s
             q[3] = .25 * s
-
-    q = q / np.linalg.norm(q) # make sure that it is unit length
 
     label = np.concatenate((location, q))
 